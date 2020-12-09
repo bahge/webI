dbcca3c87f2008b7f66011db05cd2ecd20b0e362
BaHGe <leandrobage@gmail.com>
2020-12-09 01:11:30 -0400

    Create da área de tarefas

--- html/app/controllers/tasks.php
+++ html/app/controllers/tasks.php
@@ -2,12 +2,147 @@
 namespace app\controllers;
 
 use app\helpers\views;
+use app\helpers\crud;
+use app\models;
 class tasks implements stdController
 {
-
+    /**
+     * Função index, renderiza a view com a lista de tarefas
+     */
     public function index()
     {
+        $read = new crud();
+        $array['states'] = $read->read(getenv('TBL_STATE'), '', '', array('id', 'description'));
+        $array['sectors'] = $read->read(getenv('TBL_SECTOR'), '', '', array('id', 'name'));
+        $array['persons'] = $read->read(getenv('TBL_PERSON'), '', '', array('id', 'name'));
+
         $page = new views('tasks/index');
         $page->render();
+    }
+
+    /**
+     * Função create, renderiza a view para inserir uma nova tarefa
+     */
+    public function create()
+    {
+        $read = new crud();
+        $array['states'] = $read->read(getenv('TBL_STATE'), '', '', array('id', 'description'));
+        $array['sectors'] = $read->read(getenv('TBL_SECTOR'), '', '', array('id', 'name'));
+        $array['persons'] = $read->read(getenv('TBL_PERSON'), '', '', array('id', 'name'));
+
+        $page = new views('tasks/create', $array);
+        $page->render();
+    }
+
+    /**
+     * Função update, renderiza a view passando os dados da tarefa para edição
+     * @param $id
+     */
+    public function update($id)
+    {
+        $read = new crud();
+        $array['states'] = $read->read(getenv('TBL_STATE'), '', '', array('id', 'description'));
+        $array['sectors'] = $read->read(getenv('TBL_SECTOR'), '', '', array('id', 'name'));
+
+        $task = new models\tasks();
+        $array['task'] = $task->listById($id);
+        if (!is_null($array['task'])){
+            $page = new views('task/update', $array);
+            $page->render();
+        } else {
+            echo '🅴🆁🆁🅾: Tarefa não encontrada';exit;
+        }
+    }
+
+    /**
+     * Função detete, renderiza a view com os dados da tarefa para confirmar a remoção do registro
+     * @param $id
+     */
+    public function delete($id)
+    {
+        $read = new crud();
+        $array['states'] = $read->read(getenv('TBL_STATE'), '', '', array('id', 'description'));
+        $array['sectors'] = $read->read(getenv('TBL_SECTOR'), '', '', array('id', 'name'));
+
+        $task = new models\tasks();
+        $array['task'] = $task->listById($id);
+        if (!is_null($array['task'])){
+            $page = new views('task/delete', $array);
+            $page->render();
+        } else {
+            echo '🅴🆁🆁🅾: Tarefa não encontrada';exit;
+        }
+    }
+
+    /**
+     * Função saveTask, recebe os dados de inserção de uma tarefa, chamando a model de manipulação do BD
+     */
+    public function saveTask()
+    {
+        if (isset($_POST['what_pa'])){
+            $what_pa = filter_input(INPUT_POST, 'what_pa', FILTER_SANITIZE_STRING);
+            $who_pa = filter_input(INPUT_POST, 'who_pa', FILTER_SANITIZE_NUMBER_INT);
+            $when_pa = filter_input(INPUT_POST, 'when_pa', FILTER_DEFAULT);
+            $why_pa = filter_input(INPUT_POST, 'why_pa', FILTER_SANITIZE_STRING);
+            $where_pa = filter_input(INPUT_POST, 'where_pa', FILTER_SANITIZE_STRING);
+            $how_pa = filter_input(INPUT_POST, 'how_pa', FILTER_SANITIZE_STRING);
+            $howMuch_pa = filter_input(INPUT_POST, 'howMuch_pa', FILTER_SANITIZE_NUMBER_FLOAT);
+            $state = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_NUMBER_INT);
+            $sector = filter_input(INPUT_POST, 'sector', FILTER_SANITIZE_NUMBER_INT);
+
+            $task = new models\tasks(null, $what_pa,$who_pa,$when_pa,$why_pa,$where_pa,$how_pa,$howMuch_pa, $state, $sector);
+            $save = $task->save($task);
+
+            if ($save == true) {
+                echo '🆂🆄🅲🅴🆂🆂🅾: Registro inserido com sucesso.';
+            } else {
+                echo '🅴🆁🆁🅾: ' .$save['Erro'];
+            }
+        }
+    }
+
+    /**
+     * Função updateTask, recebe os dados de edição de uma tarefa, chamando a model de manipulação do BD
+     */
+    public function updateTask()
+    {
+        if (isset($_POST['id'])){
+            $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+            $what_pa = filter_input(INPUT_POST, 'what_pa', FILTER_SANITIZE_STRING);
+            $who_pa = filter_input(INPUT_POST, 'who_pa', FILTER_SANITIZE_NUMBER_INT);
+            $when_pa = filter_input(INPUT_POST, 'when_pa', FILTER_DEFAULT);
+            $why_pa = filter_input(INPUT_POST, 'why_pa', FILTER_SANITIZE_STRING);
+            $where_pa = filter_input(INPUT_POST, 'where_pa', FILTER_SANITIZE_STRING);
+            $how_pa = filter_input(INPUT_POST, 'how_pa', FILTER_SANITIZE_STRING);
+            $howMuch_pa = filter_input(INPUT_POST, 'howMuch_pa', FILTER_SANITIZE_NUMBER_FLOAT);
+            $state = filter_input(INPUT_POST, 'state', FILTER_SANITIZE_NUMBER_INT);
+            $sector = filter_input(INPUT_POST, 'sector', FILTER_SANITIZE_NUMBER_INT);
+
+            $task = new models\tasks($id, $what_pa,$who_pa,$when_pa,$why_pa,$where_pa,$how_pa,$howMuch_pa, $state, $sector);
+            $save = $task->save($task);
+
+            if ($save == true) {
+                echo '🆂🆄🅲🅴🆂🆂🅾: Registro inserido com sucesso.';
+            } else {
+                echo '🅴🆁🆁🅾: ' .$save['Erro'];
+            }
+        }
+    }
+
+    /**
+     * Função deleteTask, recebe os dados para remoção de uma tarefa, após a confirmação, chamando a model de manipulação do BD
+     */
+    public function deleteTask()
+    {
+        if (isset($_POST['id'])){
+            $id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+            $task = new models\tasks();
+            $delete = $task->deleteById($id);
+            if ($delete == true) {
+                echo '🆂🆄🅲🅴🆂🆂🅾: Registro removido com sucesso.';
+            } else {
+                echo '🅴🆁🆁🅾: ' .$delete['Erro'];
+            }
+        }
     }
 }
--- html/app/views/tasks/create.php
+++ html/app/views/tasks/create.php
@@ -1,1 +1,40 @@
 <?php
+    $persons = $this->data['persons'];
+?>
+<ul class="breadcrumb">
+    <li><a href="<?php echo getenv("URLBASE"); ?>/state">Tarefas</a></li>
+    <li>Nova Tarefa</li>
+</ul>
+<div class="row">
+    <div class="col-1">
+        <form id="sendTask">
+            <label for="what_pa">O que ?</label><br>
+            <textarea id="what_pa" name="what_pa"></textarea>
+            <div class="select">
+                <label for="who_pa">Quem ?</label><br>
+                <select name="who_pa" id="who_pa">
+                    <?
+                    foreach ($persons as $person){
+                        echo '<option value="'.$person['id'].'">'.$person['name'].'</option>';
+                    }
+                    ?>
+                </select>
+            </div>
+            <label for="when_pa">Quando ?</label><br>
+            <input id="when_pa" type="date" name="when_pa"><br>
+            <label for="why_pa">Por quê ?</label><br>
+            <textarea id="why_pa" name="why_pa"></textarea>
+            <label for="where_pa">Onde ?</label><br>
+            <input id="where_pa" type="text" name="where_pa"><br>
+            <input class="btn btn-primary" type="button" onclick="validaForm()" value="Enviar" name="submit" id="btnsend">
+        </form>
+    </div>
+</div>
+
+<script>
+    function validaForm(){
+        var description = document.getElementById('description');
+        if (description.value === ''){ alert('O campo descrição não pode estar em branco!'); return; }
+        sendForm('sendTask', '<?= getenv('URLBASE');?>/state/saveState');
+    }
+</script>
